280d9156031f64409eef5082a9b6432e48239e4f
arorajasman <arorajasman97@gmail.com>
2023-10-06 11:16:07 +0530

    added filtering features for TODOs

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -35,7 +35,58 @@ const getAllTodos = async (req, res, next) => {
   try {
     // get id of the current logged in user
     const user = req.user.id;
-    const todos = await Todo.find({ user });
+
+    // querying
+    let queryObject = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    if (queryObject) {
+      excludedFields.forEach((element) => delete queryObject[element]);
+    }
+    let queryString = JSON.stringify(queryObject);
+    if (queryString) {
+      queryString = queryString.replace(
+        /\b(gte|gt|lte|lt)\b/g,
+        (match) => `$${match}`
+      );
+    }
+    let queryJSON;
+    if (queryString) {
+      queryJSON = JSON.parse(queryString);
+    }
+    let todosQuery = queryJSON
+      ? Todo.find({ ...queryJSON, user })
+      : Todo.find({ user });
+
+    // sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      todosQuery = todosQuery.sort(sortBy);
+    } else {
+      // sorting in descending order by createdAt
+      todosQuery = todosQuery.sort("-createdAt");
+    }
+
+    // Field Limiting or projecting
+    if (req.query.fields) {
+      const selectedFields = req.query.split(",").join(" ");
+      todosQuery = todosQuery.select(selectedFields);
+    } else {
+      // by default getting the data without __v field
+      todosQuery = todosQuery.select("-__v");
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1; // converting the string to number
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    if (req.query.page) {
+      // getting the number of documents from the database
+      const numberOfTours = await Todo.countDocuments();
+      if (skip >= numberOfTours) throw new Error("This page does not exists");
+    }
+    todosQuery = todosQuery.skip(skip).limit(limit);
+
+    const todos = await todosQuery;
     if (!todos) {
       const err = new CustomError(
         `No Task found for the user`,
